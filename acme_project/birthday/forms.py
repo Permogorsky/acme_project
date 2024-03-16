6bcdbb13746ffe13586c4c5fe111303f2dd7e29d
Павел Пермогорский <pavel.permogorskii@yandex.tu>
2024-03-16 21:30:55 +0300

    after 9 lesson

--- acme_project/birthday/forms.py
+++ acme_project/birthday/forms.py
@@ -1,9 +1,12 @@
 # birthday/forms.py
 from django import forms
 
+from django.core.exceptions import ValidationError
 # Импортируем класс модели Birthday.
 from .models import Birthday
 
+# Множество с именами участников Ливерпульской четвёрки.
+BEATLES = {'Джон Леннон', 'Пол Маккартни', 'Джордж Харрисон', 'Ринго Старр'}
 
 # Для использования формы с моделями меняем класс на forms.ModelForm.
 class BirthdayForm(forms.ModelForm):
@@ -17,4 +20,21 @@ class BirthdayForm(forms.ModelForm):
         fields = '__all__'
         widgets = {
             'birthday': forms.DateInput(attrs={'type': 'date'})
-        } 
+        }
+
+    def clean_first_name(self):
+        first_name = self.cleaned_data['first_name']
+
+        return first_name.split()[0]
+    
+    def clean(self):
+        
+        super().clean()
+        first_name = self.cleaned_data['first_name']
+        last_name = self.cleaned_data['last_name']
+        # Проверяем вхождение сочетания имени и фамилии во множество имён.
+        if f'{first_name} {last_name}' in BEATLES:
+            raise ValidationError(
+                'Мы тоже любим Битлз, но введите, пожалуйста, настоящее имя!'
+            ) 
+
--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,12 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
 
 from .models import Birthday
 from .forms import BirthdayForm
 from .utilits import calculate_birthday_countdown
 
 
-def birthday(request):
-    form = BirthdayForm(request.POST or None)
+def birthday(request, pk=None):
+    if pk is not None:        
+        instance = get_object_or_404(Birthday, pk=pk)    
+    else:        
+        instance = None   
+    form = BirthdayForm(request.POST or None, instance=instance)
     context = {'form': form}
     if form.is_valid():
         form.save()
@@ -14,6 +18,16 @@ def birthday(request):
             form.cleaned_data['birthday']
         )
         context.update({'birthday_countdown': birthday_countdown})
+    return render(request, 'birthday/birthday.html', context)
+
+
+def delete_birthday(request, pk):
+    instance = get_object_or_404(Birthday, pk=pk)
+    form = BirthdayForm(instance=instance)
+    context = {'form': form}    
+    if request.method == 'POST':        
+        instance.delete()        
+        return redirect('birthday:list')
     return render(request, 'birthday/birthday.html', context)
 
 
