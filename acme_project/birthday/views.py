3c348bddfe4ac7ef9f1400420c4dec59d8a371ab
Павел Пермогорский <pavel.permogorskii@yandex.tu>
2024-03-16 17:03:57 +0300

    start of 8 ltccon

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,23 +1,25 @@
-# birthday/views.py
 from django.shortcuts import render
 
-# Импортируем класс BirthdayForm, чтобы создать экземпляр формы.
+from .models import Birthday
 from .forms import BirthdayForm
 from .utilits import calculate_birthday_countdown
 
 
 def birthday(request):
-    form = BirthdayForm(request.GET or None)
+    form = BirthdayForm(request.POST or None)
     context = {'form': form}
     if form.is_valid():
-        # ...вызовем функцию подсчёта дней:
+        form.save()
         birthday_countdown = calculate_birthday_countdown(
-            # ...и передаём в неё дату из словаря cleaned_data.
             form.cleaned_data['birthday']
         )
-        # Обновляем словарь контекста: добавляем в него новый элемент.
         context.update({'birthday_countdown': birthday_countdown})
-    #form = BirthdayForm()
-    
-    # Указываем нужный шаблон и передаём в него словарь контекста.
     return render(request, 'birthday/birthday.html', context)
+
+
+def birthday_list(request):
+    # Получаем все объекты модели Birthday из БД.
+    birthdays = Birthday.objects.all()
+    # Передаём их в контекст шаблона.
+    context = {'birthdays': birthdays}
+    return render(request, 'birthday/birthday_list.html', context)
